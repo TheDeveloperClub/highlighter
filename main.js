c22c9802f22f0d8fbab390eace6657a967d45ed4
AntonBaas <32677929+AntonBaas@users.noreply.github.com>
2018-04-24 16:48:21 +0200

    God's Creation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function init(){
 	console.log("Welcome!")
-	text.innerHTML = (text.innerText).replace(/./g, (a)=>{return "<span class=giraffe>"+a+"</span>"})
+	text.innerHTML = (text.innerText).replace(/./g, (a,o)=>{return "<span class=giraffe id="+o+">"+a+"</span>"})
+	text.onmouseup = (x)=>{console.log(1)}
 }
