af57157708998b20f8fb8f301ed14f8c9daea34d
AntonBaas <32677929+AntonBaas@users.noreply.github.com>
2018-04-24 17:00:11 +0200

    comment on anchorNode

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ function init(){
 	console.log("Welcome!")
 	text.innerHTML = (text.innerText).replace(/./g, (a,o)=>{return "<span class=giraffe id="+o+">"+a+"</span>"})
 	text.onmouseup = (x)=>{console.log(1)}
+	//window.getSelection().anchorNode.parentElement.style.background = "#00ff00"
 }
